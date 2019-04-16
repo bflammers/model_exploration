38bcc112fb4dd41fb7e91a017721e111d13fdd7e
Bart <bart.f.lammers@gmail.com>
2019-04-16 16:11:02 +0200

    Started adding time

--- apps/PyOD_benchmark/app.R
+++ apps/PyOD_benchmark/app.R
@@ -6,7 +6,9 @@ library(shiny)
 # Read ROC results --> not always optimal with unbalanced data
 df_auc <- read.csv('data/roc_results.csv')
 # Read Precision in top N observations --> better for anomaly detection 
-df_pn <- read.csv('data/precision_topN_results.csv')
+df_pn <- read.csv('data/topn_results.csv')
+# Read ROC results --> not always optimal with unbalanced data
+df_time <- read.csv('data/time_results.csv')
 
 vars <- c('nSamples', 'nDimensions', 'OutlierPerc')
 
@@ -23,7 +25,9 @@ ui <- bootstrapPage(
     tabsetPanel(
       tabPanel("Boxplot", plotOutput("boxplot")),
       tabPanel("Scatter plot", plotOutput("scatterplot")),
-      tabPanel("Facet plot", plotOutput("facetplot"))
+      tabPanel("Facet plot", plotOutput("facetplot")),
+      tabPanel("Time Boxplot", plotOutput("time_boxplot")),
+      tabPanel("Time Facetplot", plotOutput("time_facetplot"))
     )
   )
 )
@@ -45,6 +49,23 @@ server <- function(input, output) {
     id_vars <- setdiff(c('Data', input$selected_var), 'None')
     
     df_plot <- df %>%
+      select(-drop_cols) %>%
+      melt(id.vars = id_vars)
+    
+    if (!state()$no_var) {
+      df_plot[,input$selected_var] <- log(df_plot[,input$selected_var], base = 12)
+    }
+    
+    return(df_plot)
+    
+  })
+  
+  df_time <- reactive({
+    
+    drop_cols <- setdiff(vars, input$selected_var)
+    id_vars <- setdiff(c('Data', input$selected_var), 'None')
+    
+    df_plot <- df_time %>%
       select(-drop_cols) %>%
       melt(id.vars = id_vars)
     
@@ -105,6 +126,38 @@ server <- function(input, output) {
     
     return ( gg )
   })
+  
+  output$time_boxplot <- renderPlot({
+    
+    gg <- ggplot(data = df_time(), aes_string(x = 'variable', y = 'value')) +
+      geom_boxplot() +
+      ggtitle(state()$plot_title)
+    
+    if (!state()$no_var) {
+      gg <- gg + 
+        geom_point(aes_string(color = input$selected_var)) +
+        scale_color_viridis_c()
+    }
+    return( gg )
+  })
+  
+  output$time_facetplot <- renderPlot({
+    
+    x_var <- ifelse(state()$no_var, 'Data', input$selected_var)
+    
+    gg <- ggplot(data = df_time(), aes_string(x = x_var, y = 'value')) +
+      geom_point() +
+      geom_smooth(method = "lm", se=FALSE, color="darkgrey") +
+      facet_wrap(~variable) +
+      ggtitle(state()$plot_title)
+    
+    if (state()$no_var) {
+      gg <- gg + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    }
+    
+    return ( gg )
+  })
+  
 }
 
 # Return a Shiny app object
