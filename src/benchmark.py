61bfa260c133585be285f7a2bbad220b8f843035
Bart <bart.f.lammers@gmail.com>
2019-04-17 17:26:54 +0200

    Imported models from PyOD, added new datasets from ODDS

--- src/benchmark.py
+++ src/benchmark.py
@@ -7,6 +7,17 @@ from time import gmtime, strftime
 import pandas as pd
 import numpy as np
 
+from pyod.models.abod import ABOD
+from pyod.models.cblof import CBLOF
+from pyod.models.feature_bagging import FeatureBagging
+from pyod.models.hbos import HBOS
+from pyod.models.iforest import IForest
+from pyod.models.mcd import MCD
+from pyod.models.ocsvm import OCSVM
+from pyod.models.pca import PCA
+
+
+
 from src.dataloading import DataLoader, download_all
 
 class BenchMark:
